67ed012174002aa89e7e9150b70717f1f332c830
Luke Marsden <me@lukemarsden.net>
2022-05-13 17:43:40 +0100

    Don't install same inside the notebook image for now

--- 03-road-signs/build-and-push.sh
+++ 03-road-signs/build-and-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xeuo pipefail
-export IMAGE=combinatorml/jupyterlab-tensorflow-opencv:0.8
+export IMAGE=combinatorml/jupyterlab-tensorflow-opencv:0.9
 docker build -t $IMAGE .
 docker push $IMAGE
